b9b16de798fe394e64e1ab1549480444cffd8b5b
syedtayab321 <tayabsajid5@gmail.com>
2025-09-27 15:46:59 +0500

    route fixed

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/task_frontend/',
-  build: {
-    outDir: 'dist',
-  },
 })
